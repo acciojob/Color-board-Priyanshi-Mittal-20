05b132b8a3d581de53dd8eb83e70d9f57628d199
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-09-17 16:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let container= document.querySelector(".container")
+for(let i=0;i<800;i++)
+	{
+		let box=document.createElement("span")
+		box.setAttribute("class","square")
+		container.append(box)
+		box.addEventListener("mouseenter",()=>{
+			box.style.backgroundColor="green"
+			setTimeout(()=>{
+				box.style.backgroundColor="white"
+			},1000)
+		})
+	}
